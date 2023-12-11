1e7f8fbd35404740f1e50b885b4e8a325a4352e5
Guyo <guyo.halake@students.moringaschool.com>
2023-12-11 22:19:30 +0300

    commit

--- lib/main.py
+++ lib/main.py
@@ -1,3 +1,30 @@
+def add_house(house_list):
+    print("Add House")
+    nHouse = input("Enter name of the house: ")
+    print("Location: ")
+    nLocation = input("Enter location of the house: ")
+    print("Number of rooms: ")
+    nRooms = input("Enter number of rooms: ")
+    print("Type of house: ")
+    nType = input("Enter the type of the house: ")
+    print("Size in square feet: ")
+    nSize = input("Enter size of the house: ")
+    house_list.append([nHouse, nLocation, nRooms, nSize, nType])
+
+def search_house(house_list):
+    print("Finding a house...")
+    keyword = input("Enter Search Term: ")
+    for house in house_list:
+        if keyword in house:
+            print("\n".join([f"{key}: {value}" for key, value in zip(["Name of house", "Location", "Number of rooms", "Size in square feet", "Type of house"], house)]))
+            print()
+
+def display_houses(house_list):
+    print("Display all houses...")
+    for house in house_list:
+        print("\n".join([f"{key}: {value}" for key, value in zip(["Name of house", "Location", "Number of rooms", "Size in square feet", "Type of house"], house)]))
+        print()
+
 def main():
     try:
         # initialize house list
@@ -10,7 +37,7 @@ def main():
 
     except FileNotFoundError:
         print("Error finding file.")
-        houseList=[]
+        houseList = []
 
     choice = 0
     while choice != 4:
@@ -19,32 +46,20 @@ def main():
         print("2) Find a house")
         print("3) Show houses")
         print("4) Quit")
-        choice = int(input())
+        try:
+            choice = int(input())
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
 
         if choice == 1:
-            print("Add House")
-            nHouse = input("Enter name of the house: ")
-            print("Location")
-            nLocation = input("Enter location of the house: ")
-            print("Number of rooms")
-            nRooms = input("Enter number of rooms: ")
-            print("Type of house")
-            nType = input("Enter the type of the house: ")
-            print("Size in square feet")
-            nSize = input("Enter size of the house: ")
-            houseList.append([nHouse, nLocation, nRooms, nSize, nType])
+            add_house(houseList)
 
         elif choice == 2:
-            print("Finding a house...")
-            keyword = input("Enter Search Term: ")
-            for house in houseList:
-                if keyword in house:
-                    print(house)
+            search_house(houseList)
 
         elif choice == 3:
-            print("Display all houses...")
-            for i in range(len(houseList)):
-                print(houseList[i])
+            display_houses(houseList)
 
         elif choice == 4:
             print("Quitting Program...")
@@ -54,7 +69,8 @@ def main():
     # Saving
     with open("houseList.db", "w") as outfile:
         for house in houseList:
-            outfile.write(",".join(house) + "\n")
+            outfile.write("\n".join([f"{key}: {value}" for key, value in zip(["Name of house", "Location", "Number of rooms", "Size in square feet", "Type of house"], house)]))
+            outfile.write("\n\n")
 
 if __name__ == "__main__":
     main()
