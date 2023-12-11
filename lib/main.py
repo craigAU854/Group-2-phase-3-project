c8140a4f34d0ece192042f58c2fa6e3a11ecd42b
Guyo <guyo.halake@students.moringaschool.com>
2023-12-11 23:01:36 +0300

    sql

--- lib/main.py
+++ lib/main.py
@@ -1,51 +1,74 @@
-def add_house(house_list):
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class House(Base):
+    __tablename__ = 'houses'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    location = Column(String)
+    rooms = Column(Integer)
+    size = Column(Integer)
+    house_type = Column(String)
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+def add_house(session):
     print("Add House")
     nHouse = input("Enter name of the house: ")
-    print("Location: ")
     nLocation = input("Enter location of the house: ")
-    print("Number of rooms: ")
-    nRooms = input("Enter number of rooms: ")
-    print("Type of house: ")
+    nRooms = int(input("Enter number of rooms: "))
+    nSize = int(input("Enter size of the house: "))
     nType = input("Enter the type of the house: ")
-    print("Size in square feet: ")
-    nSize = input("Enter size of the house: ")
-    house_list.append([nHouse, nLocation, nRooms, nSize, nType])
 
-def search_house(house_list):
+    new_house = House(name=nHouse, location=nLocation, rooms=nRooms, size=nSize, house_type=nType)
+    session.add(new_house)
+    session.commit()
+
+def search_house(session):
     print("Finding a house...")
     keyword = input("Enter Search Term: ")
-    for house in house_list:
-        if keyword in house:
-            print("\n".join([f"{key}: {value}" for key, value in zip(["Name of house", "Location", "Number of rooms", "Size in square feet", "Type of house"], house)]))
-            print()
-
-def display_houses(house_list):
-    print("Display all houses...")
-    for house in house_list:
-        print("\n".join([f"{key}: {value}" for key, value in zip(["Name of house", "Location", "Number of rooms", "Size in square feet", "Type of house"], house)]))
+    houses = session.query(House).filter(House.name.like(f"%{keyword}%")).all()
+    for house in houses:
+        print("\n".join(f"{key}: {getattr(house, key)}" for key in House.__table__.columns.keys()))
         print()
 
-def main():
-    try:
-        # initialize house list
-        houseList = []
-        with open('houseList.db', "r") as infile:
-            line = infile.readline().strip()
-            while line:
-                houseList.append(line.split(","))
-                line = infile.readline().strip()
+def display_houses(session):
+    print("Display all houses...")
+    houses = session.query(House).all()
+    for house in houses:
+        print("\n".join(f"{key}: {getattr(house, key)}" for key in House.__table__.columns.keys()))
+        print()
 
-    except FileNotFoundError:
-        print("Error finding file.")
-        houseList = []
+def delete_house(session):
+    print("Delete a house...")
+    house_id = int(input("Enter the ID of the house you want to delete: "))
+    house = session.query(House).filter_by(id=house_id).first()
+    if house:
+        session.delete(house)
+        session.commit()
+        print("House deleted successfully.")
+    else:
+        print("House not found.")
+
+def main():
+    engine = create_engine('sqlite:///houseList.db', echo=True)
+    create_tables(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     choice = 0
-    while choice != 4:
+    while choice != 5:
         print("***Welcome to Amana Realty***")
         print("1) Add a house")
         print("2) Find a house")
         print("3) Show houses")
-        print("4) Quit")
+        print("4) Delete a house")
+        print("5) Quit")
+
         try:
             choice = int(input())
         except ValueError:
@@ -53,24 +76,21 @@ def main():
             continue
 
         if choice == 1:
-            add_house(houseList)
+            add_house(session)
 
         elif choice == 2:
-            search_house(houseList)
+            search_house(session)
 
         elif choice == 3:
-            display_houses(houseList)
+            display_houses(session)
 
         elif choice == 4:
+            delete_house(session)
+
+        elif choice == 5:
             print("Quitting Program...")
 
     print('Program Terminated')
-
-    # Saving
-    with open("houseList.db", "w") as outfile:
-        for house in houseList:
-            outfile.write("\n".join([f"{key}: {value}" for key, value in zip(["Name of house", "Location", "Number of rooms", "Size in square feet", "Type of house"], house)]))
-            outfile.write("\n\n")
 
 if __name__ == "__main__":
     main()
