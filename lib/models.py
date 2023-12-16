c543d698e5151f9ce26570ecbe28dcae4f80127e
clairemuiru1 <muiruclaire@gmail.com>
2023-12-16 18:34:02 +0300

    "Done with creating tables"

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,5 @@
-
 # models.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint , Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -14,5 +13,25 @@ class House(Base):
     name = Column(String)
     location = Column(String)
     rooms = Column(Integer)
+    price = Column(Float)
+    area_sqft = Column(Integer)
     size = Column(Integer)
     house_type = Column(String)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    __table_args__ = (
+        UniqueConstraint('email', name='user_email'),
+    )
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    email = Column(String(55))
+    no_of_houses = Column(Integer)
+
+class Agent(Base):
+    __tablename__ = 'agents'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String(55))
+    phone_number = Column(String)
